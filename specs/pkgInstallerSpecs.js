68a66591fe98f43ba47fb439f7339ee96729b00a
ap94sd24 <adampan94@gmail.com>
2017-06-13 23:13:41 -0700

    Initial Tests...with single,multiple and out of order packages

--- specs/pkgInstallerSpecs.js
+++ specs/pkgInstallerSpecs.js
@@ -1,1 +1,24 @@
- 
+ describe('Package Installer Test Suite', function() {
+   describe('passes with:', function() {
+   	  it('A single valid package', function() {
+   	  	var pkg = ['x:']; 
+   	  	expect(new PackageInstaller(pkg).build()).toEqual('x'); 
+   	  }); 
+
+   	  it('Packages with words and spaces', function() {
+   	  	var pkg = [
+          'KittenService: CamelCaser', 
+          'CamelCaser: '
+   	  	];
+   	  	expect(new PackageInstaller(pkg).build()).toEqual('CamelCaser, KittenService');
+   	  });
+
+   	  it('Longer out of order packages', function() {
+        var input = [
+          'KittenService: ','Leetmeme: Cyberportal','Cyberportal: Ice','CamelCaser: KittenService',
+          'Fraudstream: Leetmeme','Ice: '
+        ]; 
+        expect(new PackageInstaller(input).build()).toEqual('KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream');  
+      });
+   })
+ });
---
+++ src/pkgInstaller.js
@@ -0,0 +1,73 @@
+var PackageInstaller = function (pkg) {
+  'use strict'
+
+   var _pkg = pkg;   
+
+    /**
+   * Topological sorting algorithm
+   * @param parsed packages
+   * @returns sorted array
+   */
+  var topologicalSort = function(parsePkgToObj) {
+    var ordered = {};
+    var output = [];
+     
+
+    Object.keys(parsePkgToObj).forEach(function(parsedPkg) {
+      sortPkg(parsedPkg, []);
+    });
+
+    function sortPkg(parsedPkg, parents) {
+      if (ordered[parsedPkg]) {
+        return;
+      }
+      parents.push(parsedPkg);
+      var pkg_val = parsePkgToObj[parsedPkg];
+      pkg_val.forEach(function(dependency) {
+        
+        sortPkg(dependency, parents);
+      });
+      ordered[parsedPkg] = true;
+      output.push(parsedPkg);
+    }
+
+    return output;
+  }
+      
+  /**
+   * Function parses array into array of objects and 
+   * returns comma separated strings
+   */
+  var parsePkgToObj = function() {
+    var output = {};
+    _pkg.forEach(function(pkg_str) {
+      var tokens = pkg_str.split(':');
+       
+
+      var pkg_val = tokens[0].trim();
+      var dependency = tokens[1].trim();
+
+       
+      if (!output[pkg_val]) {
+        output[pkg_val] = [];
+      }
+      if (dependency.length > 0  && !output[dependency]) {
+        output[dependency] = [];
+      }
+      if (dependency.length > 0) {
+        output[pkg_val].push(dependency);
+      }
+    });
+    return output;
+  }
+
+ 
+
+  return {
+    pkg: _pkg,
+    build: function() {
+      var parsedPkg = parsePkgToObj();
+      return topologicalSort(parsedPkg).join(", ");
+    }
+  };
+};
